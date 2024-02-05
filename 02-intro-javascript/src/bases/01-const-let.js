4bece31a103d51fd7f9dd53f805a6d16e6e1a210
Kevin Cordero <ismael-3310@hotmail.com>
2024-02-05 10:39:45 -0500

    First Commit

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+/**
+ * Variables and Constants
+ * differences between const and let
+ * Const It is a value that does not change
+ * Let It is a value that can change
+ */
+const name="Wonejo";
+const lastName="92";
+
+let dice=5;
+dice=3
+
+console.log(name,lastName,dice)
+
+if(true){
+    const name="Mack"
+    let dice=6
+    console.log(dice)
+}
---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,1 @@
+console.log("Hello Wonejo 92")
