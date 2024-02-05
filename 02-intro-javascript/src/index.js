65bb9e1729d38481219a99da3a0e6875f4a4dc88
Kevin Cordero <ismael-3310@hotmail.com>
2024-02-05 14:53:45 -0500

    Updates

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,22 @@
-console.log("Wonejo92")
+/**
+ *  They are also know as dictionaries
+ */
+const person={
+    name:"Tony",
+    lastName: "Stark",
+    age: 45,
+    address:{
+        city: "NY",
+        zip: 1053062,
+        lat: 14.3223,
+        lng: 34.6597,
+    }
+};
+//console.table({person})
+console.log(person)
+
+/**
+ * This is a reference assignment
+ */
+const person2={...person}
+console.log(person2)
