31f25ef4ec291924388a710e729e183f38c29c9a
Kevin Cordero <ismael-3310@hotmail.com>
2024-02-25 08:18:26 -0500

    Hello World in react with Vite

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import reactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+function App(){
+    return (<h1>Hello World!!</h1>);
+}
+
+reactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 )
