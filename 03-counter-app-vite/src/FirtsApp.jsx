eac88aaaed90a27b56ca99d5b59ea0d653bd0c15
Kevin Cordero <ismael-3310@hotmail.com>
2024-03-02 04:58:46 -0500

    Css example

---
+++ 03-counter-app-vite/src/FirtsApp.jsx
@@ -0,0 +1,9 @@
+
+export const FirtsApp = () => {
+  return (
+    <>
+    <code>Wonejo</code>
+    <p>92</p>
+    </>
+  )
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import reactDOM from 'react-dom/client'
 import { App } from './HelloWorldApp'
+import { FirtsApp } from './FirtsApp'
+import './styles.css';
+
 
 
 reactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <FirtsApp/>
     </React.StrictMode>
 )
